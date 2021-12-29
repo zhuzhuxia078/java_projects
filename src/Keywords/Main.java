4709d28a2e76612c7a2f4834fe7cccabbc455858
zhuzhuxia078 <zz635186056@gmail.com>
2021-12-28 20:03:49 -0800

    Initial commit

---
+++ src/Keywords/Main.java
@@ -0,0 +1,14 @@
+package Keywords;
+
+public class Main {
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometers;
+        double kilometres = (100 * 1.609344);
+
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("this is an expression");
+    }
+    }
+}
---
+++ src/acadyme/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package acadyme.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myValue = 10000;
+
+        int myMin = Integer.MIN_VALUE;
+        int myMax = Integer.MAX_VALUE;
+        System.out.println("Minimum = " + myMin);
+        System.out.println("Maximum = " + myMax);
+    }
+}
