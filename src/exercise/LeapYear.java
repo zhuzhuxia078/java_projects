a4a945ed3df03374a6e5c200a2dd9140e44a0d83
zhuzhuxia078 <zz635186056@gmail.com>
2021-12-29 16:23:46 -0800

    LeapYear

---
+++ src/exercise/LeapYear.java
@@ -0,0 +1,24 @@
+package exercise;
+
+public class LeapYear {
+    public static boolean isLeapYear (int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        }
+
+        if (year % 4 != 0) {
+            return false;
+        }
+
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+}
+
