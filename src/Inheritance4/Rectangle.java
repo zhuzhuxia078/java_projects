07d9adb9664921216c8f99201e24fd10fec73316
zhuzhuxia078 <zz635186056@gmail.com>
2022-01-15 18:39:58 -0800

    Inheritance4

---
+++ src/Inheritance4/Rectangle.java
@@ -0,0 +1,30 @@
+package Inheritance4;
+
+public class Rectangle {
+    private double width, length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+
+        if (width < 0) {
+            this.width = 0;
+        }
+
+        if (length < 0) {
+            this.length = 0;
+        }
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+}
