9fbf05e7a3894e8ad9e359a9512f7749d882e324
zhuzhuxia078 <zz635186056@gmail.com>
2022-01-18 21:05:34 -0800

    encapsulation

---
+++ Encapsulation2/Encapsulation2/Main.java
@@ -0,0 +1,16 @@
+package Encapsulation2;
+
+public class Main {
+    public static void main(String[] args) {
+        //create a class and demonstrate proper encapsulation techniques
+        //the class will be called printer
+        //it will simulate a real computer printer
+        Printer printer = new Printer(50, false);
+        System.out.println("initial page count = " + printer.getPagesPrinted());
+
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer " + printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer " + printer.getPagesPrinted());
+    }
+}
